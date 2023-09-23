10199e7504fd82c00dbfdda64cec8d85ebe8177a
zdenkotraste <zhraste98@gmail.com>
2023-09-23 15:00:04 -0300

    removed/added files

---
+++ Vagrant/bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Docker install
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+#Use docker without sudo
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+newgrp docker
+
+#Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#Install minikube
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+
+#Install ArgoCD CLI
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+#Clone repo
+
+git clone 
